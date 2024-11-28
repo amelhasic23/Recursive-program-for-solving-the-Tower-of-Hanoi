a8435d636b0b51ace313d9e6711565a653b8c36c
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 09:22:06 +0100

    Commit

---
+++ Rekurzivni program za rešavanje Hanojske kule/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Hanoj(int n, char izvor, char cilj, char pomocni)
+    {
+        if (n == 1)
+        {
+            Console.WriteLine($"Premesti disk 1 sa {izvor} na {cilj}");
+            return;
+        }
+
+        Hanoj(n - 1, izvor, pomocni, cilj); // Premesti n-1 diskova na pomoćni stub
+        Console.WriteLine($"Premesti disk {n} sa {izvor} na {cilj}");
+        Hanoj(n - 1, pomocni, cilj, izvor); // Premesti n-1 diskova na cilj
+    }
+
+    static void Main()
+    {
+        Console.Write("Unesite broj diskova: ");
+        int brojDiskova = int.Parse(Console.ReadLine());
+        Hanoj(brojDiskova, 'A', 'C', 'B'); // Stubovi su A (izvor), C (cilj), B (pomoćni)
+    }
+}
+
---
+++ Rekurzivni program za rešavanje Hanojske kule/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Rekurzivni program za rešavanje Hanojske kule")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rekurzivni program za rešavanje Hanojske kule")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64823a17-93a6-4569-813b-794ce33dd215")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
